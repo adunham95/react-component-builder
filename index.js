0f102aee169ce12058bf21700e98c9fcae9eb429
Adrian <adunham95@gmail.cooom>
2021-04-01 07:32:17 -0400

    Updated to cli

--- index.js
+++ index.js
@@ -1,69 +1,68 @@
-#!/usr/bin/env node
-
 const { program } = require('commander');
 const inquirer = require('inquirer');
 const { buildComponent } = require('./buildComponent');
 const { getVersion} = require("./getMeta")
 
-program
-    .version(getVersion())
-    .option('-d, --debug', 'debugging')
-    .option('-t, --test', 'Added for testing to bypass questions')
+module.exports = function(){
+    program
+        .version(getVersion())
+        .option('-d, --debug', 'debugging')
+        .option('-t, --test', 'Added for testing to bypass questions')
 
-program.parse(process.argv);
+    program.parse(process.argv);
 
-const questions = [
-    {
-        type : "input",
-        name : `name`,
-        message : "Component Name: "
-    },
-    {
-        type : "input",
-        name : `path`,
-        default: "/src/components",
-        message : "Component Path: "
-    },
-    {
-        type : "list",
-        name : `fileType`,
-        message : "File Type: ",
-        choices: [{name:"javascript", checked: true}, "typescript"]
-    },
-    {
-        type : "checkbox",
-        name : `files`,
-        message : "Component Files: ",
-        choices: [ {name: "jsx", checked: true},"test", "scss", "css" ]
-    },
-]
+    const questions = [
+        {
+            type : "input",
+            name : `name`,
+            message : "Component Name: "
+        },
+        {
+            type : "input",
+            name : `path`,
+            default: "/src/components",
+            message : "Component Path: "
+        },
+        {
+            type : "list",
+            name : `fileType`,
+            message : "File Type: ",
+            choices: [{name:"javascript", checked: true}, "typescript"]
+        },
+        {
+            type : "checkbox",
+            name : `files`,
+            message : "Component Files: ",
+            choices: [ {name: "jsx", checked: true},"test", "scss", "css" ]
+        },
+    ]
 
-const options = program.opts();
-if (options.debug) console.log(options);
+    const options = program.opts();
+    if (options.debug) console.log(options);
 
-if(options.test){
-    const testData = {
-        name: 'Test Component',
-        path: '/src/components',
-        fileType: 'javascript',
-        files: [ 'jsx','scss','test' ]
-      }
-    buildComponent(testData, true)
-}
-else{
-    inquirer
-    .prompt(questions)
-    .then(answers => {
-        // console.log(answers)
-        buildComponent(answers, options.debug)
-        // Use user feedback for... whatever!!
-    })
-    .catch(error => {
-        if(error.isTtyError) {
-        // Prompt couldn't be rendered in the current environment
-        } else {
-        // Something else went wrong
+    if(options.test){
+        const testData = {
+            name: 'Test Component',
+            path: '/src/components',
+            fileType: 'javascript',
+            files: [ 'jsx','scss','test' ]
         }
-    });
-
+        buildComponent(testData, true)
+    }
+    else{
+        inquirer
+        .prompt(questions)
+        .then(answers => {
+            // console.log(answers)
+            buildComponent(answers, options.debug)
+            // Use user feedback for... whatever!!
+        })
+        .catch(error => {
+            if(error.isTtyError) {
+            // Prompt couldn't be rendered in the current environment
+            } else {
+            // Something else went wrong
+            }
+        });
+    }
 }
